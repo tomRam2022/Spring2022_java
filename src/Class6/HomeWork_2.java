1bd2a0c2b01fe3c10f08c2e88fccfe944b91a1c1
Tomas Ramirez <you@example.com>
2022-03-06 23:33:23 -0500

    Adding homework 2

---
+++ src/Class6/HomeWork_2.java
@@ -0,0 +1,50 @@
+package Class6;
+
+public class HomeWork_2 {
+
+    public static void main(String[] args) {
+
+        /**
+         *
+         * Due: Mar 6 (Sunday)
+         *
+         * Store values for below scenario in an appropriate variable, and print the same in Console.
+         *  1. number of hours in a day
+         *  2. max no. of days in an year
+         *  3. total number of employees in an organization
+         *  4. Population in a country
+         *  5. Mortgage rate
+         *  6. Balance in an bank account
+         *  7. does the sun rise from the West?
+         *  8. Initials of your name (Use: char)    // My initials are DS
+         *  9. Store your full name (Use: String)
+         */
+
+        int numOfHrsInDay = 24;
+        int maxNumOfDaysInYear = 365;
+        int totalNumOfEmployeesInPayrollDept = 10;
+        int populationInVaticanCity = 825;
+        double myMortgageRate = 5.5;
+        double myBankAcctBal = 100;
+        boolean doesSunRiseWest = false;
+        char firstNameInt = 'T';
+        char lastNameInt = 'R';
+        String myName = "Tomas Ramirez";
+
+
+        System.out.println();
+        System.out.println("Number of hours in a day --> " + numOfHrsInDay);
+        System.out.println("Max numbers of days in a year --> " + maxNumOfDaysInYear);
+        System.out.println("Total number of employees in the payroll department --> " + totalNumOfEmployeesInPayrollDept);
+        System.out.println("population of vatican city --> " + populationInVaticanCity);
+        System.out.println("My current mortgage rate in 2022 -->" + myMortgageRate);
+        System.out.println("My current bank account balance as of March 1 2022 --> " + myBankAcctBal);
+        System.out.println("Does the sun rise from the west ? " + doesSunRiseWest);
+        System.out.println("My initials are --> " + firstNameInt + lastNameInt);
+        System.out.println("My full name --> " + myName);
+
+    }
+
+
+
+}
